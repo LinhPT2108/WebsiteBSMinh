abfb0b0920cbfcb636dccd6bdc99160f3df4f3d0
LinhPT2108 <tuanlinho9o22o15@gmail.com>
2023-12-14 08:47:32 +0700

    update fix ui social

--- src/components/socialmedia/socialmedia.lienhe.tsx
+++ src/components/socialmedia/socialmedia.lienhe.tsx
@@ -2,11 +2,17 @@ import Link from "next/link";
 import { Image } from "react-bootstrap";
 const SocialmediaComponent = () => {
   return (
-    <div className="social-media d-flex justify-content-around flex-column">
-      <Link href="tel:1231231231" className="social-media--icon">
+    <div className="social-media d-flex justify-content-around flex-column py-0">
+      <Link
+        href="tel:1231231231"
+        className="social-media--icon d-flex justify-content-center"
+      >
         <i className="bi bi-telephone-forward"></i>
       </Link>
-      <Link href="https://zalo.me/0913662553">
+      <Link
+        href="https://zalo.me/0913662553"
+        className="social-media--icon d-flex justify-content-center align-items-center"
+      >
         <Image
           src="https://sanphukhoatudu-bsdiep.com//wp-content/themes/thammyvientsnhan/img/icon-zalo.png"
           alt="icon zalo"
@@ -14,7 +20,10 @@ const SocialmediaComponent = () => {
         ></Image>
       </Link>
 
-      <Link href="#" className="social-media--icon">
+      <Link
+        href="#"
+        className="social-media--icon d-flex justify-content-center"
+      >
         <i className="bi bi-messenger"></i>
       </Link>
     </div>
